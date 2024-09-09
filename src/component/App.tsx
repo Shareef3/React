b9df4c6649ff716ece77ac286a0b19a3251b9f09
Shareef3 <shareefmeeravali@gmail.com>
2024-09-09 16:23:25 +0530

    React

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,13 +1,14 @@
-
+import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './Login'
 import AboutUs from './AboutUs'
 import AddNewMovie from './AddNewMovie'
 import AllMoviesList from './AllMovieList'
 import ContactUs from './ContactUs'
 import EditMovie from './EditMovie'
 import Home from './Home'
+import Login from './Login'
 import MovieDescription from './MovieDescription'
+import Register from './Register'
 
 export default function App() {
   return (
@@ -18,11 +19,12 @@ export default function App() {
 <Route path="/" element={<Login />} />
 <Route path="/home" element={<Home />} />
 <Route path="/add" element={<AddNewMovie />} />
-<Route path="/edit" element={<EditMovie />} />
+<Route path="/edit/:id" element={<EditMovie />} />
 <Route path="/all" element={<AllMoviesList />} />
 <Route path="/about" element={<AboutUs />} />
 <Route path="/contact" element={<ContactUs />} />
-<Route path="/mDes" element={<MovieDescription />} />
+<Route path="/mDes/:id" element={<MovieDescription />} />
+{/* <Route path="/register" element={<Register />} /> */}
 
 
 </Routes>
--- src/component/Login.tsx
+++ src/component/Login.tsx
@@ -2,49 +2,82 @@
 
 import { useNavigate } from 'react-router-dom'
 import '../assets/css/login.css'
+import axios from "axios";
+import { useState, FormEvent } from 'react';
 
 export default function Login() {
 
-    const nav = useNavigate();
+   const nav = useNavigate();
 
-    const loginSubmit =()=>{
+   const [message, setMessage] = useState('');
+   const [name, setName] = useState("");
+   const [password, setPassword] = useState("");
 
-        nav('/home');
-    }
-  return (
-    <div>
-        	<div className="sidenav">
-         <div className="login-main-text">
-            <h2>Application Login Page</h2>
-            <p>Login or register from here to access.</p>
-         </div>
-      </div>
-      <div className="main">
-         <div className="col-md-6 col-sm-12">
-            <div className="login-form">
-               <form onSubmit={loginSubmit}>
-                  <div className="form-group">
-                     <label>User Name</label>
-                     <input type="text" className="form-control" placeholder="User Name"/>
-                  </div>
-                  <div className="form-group">
-                     <label>Password</label>
-                     <input type="password" className="form-control" placeholder="Password"/>
-                  </div>
-                  <button type="submit" className="btn btn-black">Login</button>
-                  <button type="submit" className="btn btn-secondary">Register</button>
-               </form>
+   const loginSubmit = (e: FormEvent) => {
+      e.preventDefault();
+      console.log("user name--->" + name);
+      console.log("user password...." + password);
+
+      const loginUser = { name, password };
+
+      axios.get("http://localhost:1234/nit/login?username=" + name + "&password=" + password)
+         .then((res) => {
+            console.log("result---->" + res.data);
+            if (res.data == "success") {
+               nav("/home");
+            }
+            else {
+               axios.post("http://localhost:1234/nit/login/add"+name+"&password="+password)
+               setMessage("Credentials are not good!");
+               setName("");
+               setPassword("");
+            }
+         });
+
+
+   }
+   return (
+      <div>
+         <div className="sidenav">
+            <div className="login-main-text">
+               <h2>Application Login Page</h2>
+               <p>Login or register from here to access.</p>
             </div>
          </div>
+         <div className="main">
+            <div className="col-md-6 col-sm-12">
+               <div className="login-form">
+                  <form onSubmit={loginSubmit}>
+                  <div>
+				          {message}
+			         </div>
+                     <div className="form-group">
+                        <label>User Name</label>
+                        <input type="text" 
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}className="form-control" placeholder="User Name" />
+                     </div>
+                     <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="form-control" placeholder="Password" />
+                     </div>
+                     <button type="submit" className="btn btn-black">Login</button>
+                     <button type="button" className="btn btn-secondary" >Register</button>
+                  </form>
+               </div>
+            </div>
+         </div>
+
+
+
+
       </div>
 
 
 
 
-	</div>
-
-
-
-
-  )
+   )
 }
